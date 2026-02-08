e4c0a244b8a5ea1c29c8fedace5af14390e9d827
DSPMSD78 <prasad.duggirala.369@gmail.com>
2026-02-08 11:23:18 +0530

    "1st Commit"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 function App() {
-  return <div className="text-3xl text-red-500">Namaste Netflix - GPT</div>;
+  return <div className="text-red-500">Namaste Netflix - GPT</div>;
 }
 export default App;
