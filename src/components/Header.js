0b8afcaafc55c11b04f3d96d250e536c704c1a79
DSPMSD78 <prasad.duggirala.369@gmail.com>
2026-02-09 18:59:51 +0530

    "firebase config updated"

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="absolute py-2 bg-gradient-to-b from-black z-10">
       {/* <img className="w-40" alt="Netflix Logo" src={logo} /> */}
       <h1
-        className="text-red-600 font-bold text-3xl tracking-tighter uppercase italic"
+        className="w-40 text-red-600 font-bold text-3xl tracking-tighter uppercase italic"
         style={{
           fontFamily: "Helvetica, Arial, sans-serif",
           transform: "scaleY(1.4) scaleX(0.9)", // Mimics the stretched Netflix look
