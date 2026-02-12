c1e61a3d6216cb9eed2ac46e6f2a44ee714246d0
DSPMSD78 <prasad.duggirala.369@gmail.com>
2026-02-12 22:54:21 +0530

    "Updated VideoBackground Component"

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,7 +11,7 @@ const VideoBackground = ({ movieId }) => {
         className="w-screen aspect-video"
         src={`https://www.youtube.com/embed/${trailerData?.key}?&autoplay=1&mute=1`}
         title="YouTube video player"
-        allow="accelerometer; autoplay"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
   );
